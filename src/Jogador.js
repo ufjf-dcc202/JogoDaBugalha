39e8aea43324bc2800230d68276c2ff54f8ca766
Miguel Dias <miguel.dias@estudante.ufjf.br>
2023-11-23 15:17:11 -0300

    tabuleiro do jogador added

--- src/Jogador.js
+++ src/Jogador.js
@@ -1,13 +1,19 @@
 
-player = {
-    pontos: [0,0,0],
-    dado:0,
-    tabuleiro:[[0,0,0],[0,0,0],[0,0,0]],
-    posicionar_dado:function(x,y,valor){
-        console.log(this)
-        this.tabuleiro[x][y]=valor;
-    },
-    jogar_dado:function(){
-        dado = Math.random(1,6)
+function Jogador(){
+    jogador = {
+        pontos: [0,0,0],
+        dado:0,
+        tabuleiro:[[0,0,0],[0,0,0],[0,0,0]],
+        posicionar_dado:function(x,y,valor){
+            console.log(this)
+            this.tabuleiro[x][y]=valor;
+        },
+        jogar_dado:function(){
+            dado = Math.random(1,6)
+        }
     }
+    return jogador;
 }
+
+
+
