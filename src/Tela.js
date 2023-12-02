583c7d14e6992abc2cb40e13ea7218e5ca6dbfc3
Miguel Dias <miguel.dias@estudante.ufjf.br>
2023-12-02 17:21:03 -0300

    função get_elementos_tabuleiro ( added)

--- src/Tela.js
+++ src/Tela.js
@@ -1,7 +1,26 @@
+function get_elementos_tabuleiro(tabuleiro,elemento) {
+    const rows = tabuleiro.children
+    let elementos = []
+    for (let x = 0; x < 4; x++) {
+        let collum = rows[x].children
+        for (let i = 0; i < collum.length; i++) {
+            const casa = collum[i];
+            if (casa.tagName==elemento) {
+                elementos.push(casa)
+            }
+            
+        }
+      
+       
+    
+    }
+    return elementos
+}
 function atualiza_tabuleiro(matriz, elemento) {
     const tabuleiro = elemento
     const rows = tabuleiro.children
     const tamanho = 3
+ 
     for (let x = 0; x < tamanho; x++) {
         let collum = rows[x].children
         for (let y = 0; y < tamanho; y++) {
@@ -17,6 +36,16 @@ function atualiza_tabuleiro(matriz, elemento) {
     }
 }
 function atualiza_dado(num, dado_div) {
+    const dado = dado_div
+//    console.log(dado)
+    dado.innerText = num
+}
+function atualiza_pontos(pontos, tabuleiro_ctn) {
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
     const dado = dado_div
     console.log(dado)
     dado.innerText = num
