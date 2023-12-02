aad96156d978663e5b6b2ded2dba177fd73b2507
Miguel Dias <miguel.dias@estudante.ufjf.br>
2023-12-02 17:29:17 -0300

    função atualiza_pontos ( added)

--- src/Tela.js
+++ src/Tela.js
@@ -7,20 +7,22 @@ function get_elementos_tabuleiro(tabuleiro,elemento) {
             const casa = collum[i];
             if (casa.tagName==elemento) {
                 elementos.push(casa)
-            }
-            
+            }   
         }
-      
-       
-    
     }
     return elementos
 }
-function atualiza_tabuleiro(matriz, elemento) {
+function atualiza_pontos(elementos,pontos) {
+    for (let i = 0; i < elementos.length; i++) {
+         elementos[i].innerText = pontos[i];
+        
+    }
+}
+function atualiza_tabuleiro(matriz, elemento, pontos) {
     const tabuleiro = elemento
     const rows = tabuleiro.children
     const tamanho = 3
- 
+    
     for (let x = 0; x < tamanho; x++) {
         let collum = rows[x].children
         for (let y = 0; y < tamanho; y++) {
@@ -37,19 +39,9 @@ function atualiza_tabuleiro(matriz, elemento) {
 }
 function atualiza_dado(num, dado_div) {
     const dado = dado_div
-//    console.log(dado)
     dado.innerText = num
 }
-function atualiza_pontos(pontos, tabuleiro_ctn) {
 
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        
-    }
-    const dado = dado_div
-    console.log(dado)
-    dado.innerText = num
-}
 function atualizar_tela(player) {
     atualiza_tabuleiro(player.tabuleiro, player.tabuleiro_div)
     atualiza_dado(player.dado, player.dado_div)
