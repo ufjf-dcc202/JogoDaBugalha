338ac320ef5d7910fb941827c4262087a5d866ed
Miguel Dias <miguel.dias@estudante.ufjf.br>
2023-12-03 16:41:13 -0300

    função calcula pontos movida para jogadorjs

--- src/Interações.js
+++ src/Interações.js
@@ -4,60 +4,16 @@ import {atualizar_tela} from './Tela.js'
 let divTabuleiroPlayer = document.getElementById('tab_player')
 let divDadoPlayer = document.getElementById('dado_player')
 let player = Jogador(divTabuleiroPlayer, divDadoPlayer)
-
 let divTabuleiroBot = document.getElementById('tab_bot')
 let divDadoBot = document.getElementById('dado_bot')
 let bot = Jogador(divTabuleiroBot, divDadoBot)
+
+
 function Jogo(player1, player2) {
     return {
         player: player1,
         bot: player2,
-        calcular_pontos: function(jogador) {
-            let somaColuna = [0, 0, 0]
-            let pontosColunas = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-            let tabuleiro = jogador.tabuleiro
-            for (let linha = 0; linha < 3; linha++) {
-                for (let coluna = 0; coluna < 3; coluna++) {
-                    pontosColunas[linha][coluna] = tabuleiro[coluna][linha]
-                }
-            }
-
-            for (let i = 0; i < 3; i++) {
-                let numRep = 0
-                let repetidos = 1
-                for (let j = 0; j < 3; j++) {
-                    if (pontosColunas[i][0] == pontosColunas[i][1] && pontosColunas[i][0] != 0 && pontosColunas[i][1] != 0) {
-                        repetidos++
-                        numRep = parseInt(pontosColunas[i][0])
-                    }
-                    if (pontosColunas[i][0] == pontosColunas[i][2] && pontosColunas[i][0] != 0 && pontosColunas[i][2] != 0) {
-                        repetidos++
-                        numRep = parseInt(pontosColunas[i][0])
-                    }
-                    if (pontosColunas[i][1] == pontosColunas[i][2] && repetidos < 3 && pontosColunas[i][1] != 0 && pontosColunas[i][2] != 0) {
-                        repetidos++
-                        numRep = parseInt(pontosColunas[i][1])
-                    }
-
-                    if (pontosColunas[i][j] == numRep && pontosColunas[i][j] != 0) {
-                        if (repetidos == 2) {
-                            somaColuna[i] += ((numRep * repetidos) * repetidos) / 2
-                        } else if (repetidos == 3) {
-                            somaColuna[i] += ((numRep * repetidos) * repetidos) / 3
-                        }
-                    } else {
-                        somaColuna[i] += pontosColunas[i][j];
-                    }
-                    numRep = 0
-                    repetidos = 1
-                }
-
-            }
-            for (let i = 0; i < 3; i++) {
-                jogador.pontos[i] = somaColuna[i];
-            }
-            jogador.pontos
-        },
+       
         checa_vitoria: function() {
             const tab_player = this.player.tabuleiro
             const tab_player2 = this.bot.tabuleiro
@@ -95,15 +51,13 @@ function Jogo(player1, player2) {
             this.player.posicionar_dado(x, y)
             this.player.jogar_dado()
             this.bot.jogada()
-            this.calcular_pontos(this.player)
-            this.calcular_pontos(this.bot)
+            
+         
             this.checa_vitoria()
-            console.log(player1.pontos)
-
             atualizar_tela(this.player)
             atualizar_tela(this.bot)
         },
-        clicar_tabuleiro: function(e) {
+        pega_posições: function(e) {
             let tabuleiro = document.getElementById('tab_player')
             let rows = tabuleiro.children
             for (let x = 0; x < 3; x++) {
@@ -133,7 +87,7 @@ function setar_click() {
             if (collum[y].tagName == 'TD') {
                 let jogo = Jogo(player, bot)
                 elemento.addEventListener("click", (e)=>{
-                    jogo.clicar_tabuleiro(e)
+                    jogo.pega_posições(e)
                 }
                 );
             }
@@ -142,3 +96,5 @@ function setar_click() {
 }
 setar_click()
 player.jogar_dado()
+atualizar_tela(player)
+
--- src/Jogador.js
+++ src/Jogador.js
@@ -13,6 +13,7 @@ function Jogador(div_tabuleiro, div_dado) {
         posicionar_dado: function(x, y) {
             let valor = this.dado
             this.tabuleiro[x][y] = valor;
+            this.calcular_pontos()
         },
         jogar_dado: function() {
             let numero_aleatorio = Math.floor(Math.random() * 6) + 1;
@@ -28,7 +29,51 @@ function Jogador(div_tabuleiro, div_dado) {
             } while (this.tabuleiro[posx][posy] != "");
             this.jogar_dado();
             this.posicionar_dado(posx, posy)
-        }
+        },
+        calcular_pontos: function() {
+            let somaColuna = [0, 0, 0]
+            let pontosColunas = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+            let tabuleiro = this.tabuleiro
+            for (let linha = 0; linha < 3; linha++) {
+                for (let coluna = 0; coluna < 3; coluna++) {
+                    pontosColunas[linha][coluna] = tabuleiro[coluna][linha]
+                }
+            }
+            for (let i = 0; i < 3; i++) {
+                let numRep = 0
+                let repetidos = 1
+                for (let j = 0; j < 3; j++) {
+                    if (pontosColunas[i][0] == pontosColunas[i][1] && pontosColunas[i][0] != 0 && pontosColunas[i][1] != 0) {
+                        repetidos++
+                        numRep = parseInt(pontosColunas[i][0])
+                    }
+                    if (pontosColunas[i][0] == pontosColunas[i][2] && pontosColunas[i][0] != 0 && pontosColunas[i][2] != 0) {
+                        repetidos++
+                        numRep = parseInt(pontosColunas[i][0])
+                    }
+                    if (pontosColunas[i][1] == pontosColunas[i][2] && repetidos < 3 && pontosColunas[i][1] != 0 && pontosColunas[i][2] != 0) {
+                        repetidos++
+                        numRep = parseInt(pontosColunas[i][1])
+                    }
+
+                    if (pontosColunas[i][j] == numRep && pontosColunas[i][j] != 0) {
+                        if (repetidos == 2) {
+                            somaColuna[i] += ((numRep * repetidos) * repetidos) / 2
+                        } else if (repetidos == 3) {
+                            somaColuna[i] += ((numRep * repetidos) * repetidos) / 3
+                        }
+                    } else {
+                        somaColuna[i] += pontosColunas[i][j];
+                    }
+                    numRep = 0
+                    repetidos = 1
+                }
+            }
+            for (let i = 0; i < 3; i++) {
+                jogador.pontos[i] = somaColuna[i];
+            }
+            jogador.pontos
+        },
     }
     return jogador;
 }
